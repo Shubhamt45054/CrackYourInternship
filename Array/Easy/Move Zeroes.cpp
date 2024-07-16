16f1d8a6ce9ce4194bdd7c19f7293b1d61e8bb8c
Shubham Tyagi <131750433+Shubhamt45054@users.noreply.github.com>
2024-07-16 13:26:24 +0530

    Create  Move Zeroes.cpp

---
+++ Array/Easy/Move Zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+        int n=arr.size();
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                swap(arr[j],arr[i]);
+                j++;
+            }
+        }
+    }
+};
