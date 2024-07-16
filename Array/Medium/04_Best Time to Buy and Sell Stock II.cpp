a01a0f519f7a2bc30005b0e98850dfd120ce1d4e
Shubham Tyagi <131750433+Shubhamt45054@users.noreply.github.com>
2024-07-16 18:57:39 +0530

    Update 04_Best Time to Buy and Sell Stock II.cpp

--- Array/Medium/04_Best Time to Buy and Sell Stock II.cpp
+++ Array/Medium/04_Best Time to Buy and Sell Stock II.cpp
@@ -1,1 +1,18 @@
 
+class Solution {
+public:
+
+  int maxProfit(vector<int>& arr) {
+        int mini=arr[0];
+        int profit=0;
+        int n=arr.size();
+        for(int i=1;i<n;i++){
+            if(arr[i]>mini){
+               profit+=arr[i]-mini;
+            }
+            mini=arr[i];
+        }
+
+        return profit;
+    }
+};
