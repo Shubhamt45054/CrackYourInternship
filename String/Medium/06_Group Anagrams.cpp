a2946a4bf80a750f4dc93a70de71828e0c7aecd9
Shubham Tyagi <131750433+Shubhamt45054@users.noreply.github.com>
2024-07-24 17:45:06 +0530

    Create 06_Group Anagrams.cpp

---
+++ String/Medium/06_Group Anagrams.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& str) {
+        vector<string> temp=str;
+        int n=str.size();
+        map<string,int> mp;
+        for(int i=0;i<n;i++){
+            sort(temp[i].begin(),temp[i].end());
+            mp[temp[i]]++;
+        }
+        int k=mp.size();
+        // cout<<k<<endl;
+        vector<vector<string>> ans(k);
+
+        map<string,int> index;
+        int j=1;
+        for(int i=0;i<n;i++){
+            if(index[temp[i]]==0){
+                index[temp[i]]=j;
+                j++;
+            }
+            
+            int idx=index[temp[i]]-1;
+            ans[idx].push_back(str[i]);
+        }
+
+
+        return ans;
+    }
+};
